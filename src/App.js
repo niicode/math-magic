d067da48b537bb09ad7e4f821261df75c7fd1e0e
niicode <niidarku66@gmail.com>
2022-09-06 12:20:29 +0000

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
-export default App;
