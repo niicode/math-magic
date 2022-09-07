37d5332d2b0fd38a40a8e5912a4f022d3b2cad95
niicode <niidarku66@gmail.com>
2022-09-07 10:31:12 +0000

    Add logi to calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
+import Keypad from './Keypad';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  componentDidMount() {
+    this.handleClick('AC');
+  }
+
+  handleClick = (buttonName) => {
+    const { total, next, operation } = this.state;
+    const data = calculate({ total, next, operation }, buttonName);
+    this.setState(data);
+  };
+
   render() {
     return (
       <div className="container">
-        <input className="num-input" type="number" />
-        <div className="calc">
-          <button type="button" className="calc-button">AC</button>
-          <button type="button" className="calc-button">+/-</button>
-          <button type="button" className="calc-button">%</button>
-          <button type="button" className="calc-button sign">+</button>
-          <button type="button" className="calc-button">7</button>
-          <button type="button" className="calc-button">8</button>
-          <button type="button" className="calc-button">9</button>
-          <button type="button" className="calc-button sign">-</button>
-          <button type="button" className="calc-button">4</button>
-          <button type="button" className="calc-button">5</button>
-          <button type="button" className="calc-button">6</button>
-          <button type="button" className="calc-button sign">x</button>
-          <button type="button" className="calc-button">1</button>
-          <button type="button" className="calc-button">2</button>
-          <button type="button" className="calc-button">3</button>
-          <button type="button" className="calc-button sign">÷</button>
-          <button type="button" className="calc-button">0</button>
-          <button type="button" className="calc-button">.</button>
-          <button type="button" className="calc-button sign">=</button>
+        <div className="main">
+          <div className="num-input">{(this.state.next === null) ? this.state.total : this.state.next}</div>
+          <Keypad keypad={['AC', '+/-', '%', '÷']} clickHandler={this.handleClick} />
+          <Keypad keypad={['7', '8', '9', 'x']} clickHandler={this.handleClick} />
+          <Keypad keypad={['4', '5', '6', '-']} clickHandler={this.handleClick} />
+          <Keypad keypad={['1', '2', '3', '+']} clickHandler={this.handleClick} />
+          <Keypad keypad={['0', '.', '=']} clickHandler={this.handleClick} />
         </div>
       </div>
     );
