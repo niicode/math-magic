038f4df0db1e3d8fbb8ff03a1f90bea0946524b2
niicode <niidarku66@gmail.com>
2022-09-13 14:18:26 +0000

    Add nagivation and pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="*" element={<h1>404: Not Found</h1>} />
+        </Routes>
       </div>
     );
   }
